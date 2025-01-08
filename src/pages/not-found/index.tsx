6f5d2672eee8a2a11e9b62a19c0e4b8e99b2d80e
CUKENGER <andreychuvashov2015@gmail.com>
2025-01-08 16:05:55 +0300

    feat: styles for not found page

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,15 +1,35 @@
-import { Link } from "react-router-dom"
-
+import SentimentDissatisfiedIcon from '@mui/icons-material/SentimentDissatisfied';
+import { Box, Button, Container, Typography } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 export const NotFound = () => {
   return (
-    <div>
-      Такой страницы не существует
-      <Link
-        to={'/'}
+    <Container
+      className="flex flex-col items-center justify-center h-[92vh] text-center"
+    >
+      <SentimentDissatisfiedIcon
+        className='mb-2 text-lg'
+      />
+      <Typography
+        variant="h4"
+        component="h1"
+        gutterBottom
       >
-        На главную
-      </Link>
-    </div>
-  )
-}
+        Ой! Страница не найдена
+      </Typography>
+      <Typography
+        variant="body1"
+        gutterBottom
+      >
+        Извините, но страница, которую вы ищите, не существует
+      </Typography>
+      <Box mt={4}>
+        <Link to="/" className='no-underline'>
+          <Button variant="contained" color="primary">
+            На главную
+          </Button>
+        </Link>
+      </Box>
+    </Container>
+  );
+};
