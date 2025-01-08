dab80ba663aa2aa5e7beb0f771e6cbca850c6c7a
CUKENGER <andreychuvashov2015@gmail.com>
2025-01-08 16:07:02 +0300

    feat: custom hook for products page

---
+++ src/pages/products/hooks/useGetProducts.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react"
+
+export const useGetProducts = <T,>(
+  getProducts: () => Promise<T[]>,
+  setProducts: (data: T[]) => void,
+  setIsProductsLoaded: (isLoaded: boolean) => void,
+  isProductsLoaded: boolean,
+) => {
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const get = async () => {
+      if (isProductsLoaded) {
+        setIsLoading(false)
+        return
+      }
+      try {
+        const newData = await getProducts()
+        setProducts(newData)
+        setIsProductsLoaded(true)
+      } catch (e) {
+        console.error("Error get data:", e)
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    get()
+  }, [getProducts, isProductsLoaded, setProducts, setIsProductsLoaded])
+
+  return {
+    isLoading
+  }
+}  
