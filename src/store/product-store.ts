e36acefb7a326b29ef1b60e6c8b077afac53090e
CUKENGER <andreychuvashov2015@gmail.com>
2025-01-14 19:33:59 +0300

    test: product-store

--- src/store/product-store.ts
+++ src/store/product-store.ts
@@ -1,7 +1,7 @@
 import { create } from 'zustand'
 import { Product } from '../types/product'
 
-interface ProductState {
+export interface ProductState {
   products: Product[]
   likedProducts: Product[]
   isProductsLoaded: boolean
