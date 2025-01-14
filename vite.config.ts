59c91450254cdbc420dcf72d1987015e458a3d5a
CUKENGER <andreychuvashov2015@gmail.com>
2025-01-14 23:15:10 +0300

    build: deploy gh-pages

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="vitest/config" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/products-test/'
-})
